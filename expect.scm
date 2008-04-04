2f9cdfda9244ca163eb326bc8a04fe90aa540c00
Will Noble <phil.noble@gmail.com>
2008-04-04 04:17:30 -0400

    Changed from char-based to glyph-based avatar in expect

--- expect.scm
+++ expect.scm
@@ -1,16 +1,17 @@
-(define avatar #\@)
+(define (expect-avatar-set! glyph) (set! avatar glyph))
+(define avatar #f)
 
-; don't use this unless you're sure the screen is going to change
+;; don't use this unless you're sure the screen is going to change
 (define (expect-generic res tries)
   (and (not (string=? res ""))
        (or (at-more?)
 	   (at-question?)
 	   (at-menu?)
-	   (and avatar ; not invisible
-		(char=? (square-char (get-coord)) avatar)
+	   (and avatar			; not invisible
+		(equal? (square-glyph (get-coord)) avatar)
 		(botl-update)
-		; often a delay before topl gets printed, so extra read if topl
-		; is empty
+		;; often a delay before topl gets printed, so extra
+		;; read if topl is empty
 		(or (not (char=? (square-char '(1 1)) #\space))
 		    (> tries 2)))
 	   (> tries 4))))
